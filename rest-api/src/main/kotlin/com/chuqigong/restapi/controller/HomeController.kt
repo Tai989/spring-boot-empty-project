ef77ee913d9babcf5c7e742410c3cfd5a8e182e8
zhuqigong <konworking@gmail.com>
2023-02-09 17:00:26 +0800

    refactor code

--- rest-api/src/main/kotlin/com/chuqigong/restapi/controller/HomeController.kt
+++ rest-api/src/main/kotlin/com/chuqigong/restapi/controller/HomeController.kt
@@ -1,8 +1,10 @@
 package com.chuqigong.restapi.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,5 +18,10 @@ class HomeController {
     @GetMapping("/sayHello")
     fun sayHello(@RequestParam("username", required = false) username: String): String {
         return if (username.isNullOrBlank()) "Hello,Anonymous" else "Hello,%s".format(username)
+    }
+    @GetMapping("/json")
+    @ResponseBody
+    fun json():String{
+        return ObjectMapper().writeValueAsString(mapOf(Pair("username","konchoo"), Pair("age",28)))
     }
 }
